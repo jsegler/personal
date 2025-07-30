c42419699a66ff2b5d161f55223b9df4ff62d809
Justin Segler <justin@justinsegler.com>
2025-07-30 14:29:30 -0500

    nit

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -60,7 +60,7 @@ const ExperienceItem = ({
             exit={{ opacity: 0, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-lg transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
 
             <motion.header
               className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
