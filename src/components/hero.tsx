0a8c0f1d697ca84650cb1ae591fccc0df261ce50
Justin Segler <justin@justinsegler.com>
2025-07-31 11:21:45 -0500

    nit

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -34,7 +34,7 @@ export const Hero = () => {
         <h1 className="font-libre md:text-[5em] text-[3em]">I'm Justin.</h1>
         <h2 className="md:text-2xl text-lg mt-4" style={{ lineHeight: 2 }}>
           Full-stack engineer turning bold ideas into beautiful, scalable
-          products — from startup MVPs to enterprise AI platforms.
+          products — from startup MVPs to enterprise platforms.
         </h2>
       </div>
       <div
